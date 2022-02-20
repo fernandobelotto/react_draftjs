d475c824cbd5a4ef68c74227247b390c5c36b9aa
fernandobosco <fbosco@dlocal.com>
2022-02-20 00:25:29 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
+import { Editor, EditorState } from 'draft-js';
+import 'draft-js/dist/Draft.css';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ border: '1px solid gray', width: '50vw' }}>
+      <MyEditor />
     </div>
-  );
+  )
 }
 
-export default App;
+
+function MyEditor() {
+  const [editorState, setEditorState] = React.useState(
+    () => EditorState.createEmpty(),
+  );
+
+  return <Editor editorState={editorState} onChange={setEditorState} />;
+}
